1af4c3f1fd04bb863ee6d4e652e395a3809ff5e9
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-11-27 20:12:00 +0000

    Added basic functionality
    Compares dictionary and string, then returns a hash with a count of most words present in both lists.
    
    Problem: words that have an apostrophe (i.e. how's) is not properly recognized.
    Proposed solution: Instead of comparing by word, compare by letter

--- index.rb
+++ index.rb
@@ -9,6 +9,12 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 def subtrings(string, dictionary)
     split_string = string.downcase.split(" ")
     final_string = split_string.map { |word| word.delete('^a-zA-Z0-9') }
+
+    dictionary.reduce({}) do |hash, word|
+        hash[word] = final_string.count(word)
+        hash.delete_if { |word, count| count == 0 }
+        hash
+    end
 end
 substrings("below", dictionary)
 substrings("Howdy partner, sit down! How's it going?", dictionary)
