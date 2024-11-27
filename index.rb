1b1be8751acb3767902e0c2101cd87bb9ef3454f
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-11-27 19:32:21 +0000

    Add split string function > > String is modified to an array of individual words with no special characters (i.e., anything that is not a letter or number)

--- index.rb
+++ index.rb
@@ -4,3 +4,11 @@
 #It should return a hash listing each substring (case insensitive) 
 #that was found in the original string and how many times it was found.
 
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def subtrings(string, dictionary)
+    split_string = string.downcase.split(" ")
+    final_string = split_string.map { |word| word.delete('^a-zA-Z0-9') }
+end
+substrings("below", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
