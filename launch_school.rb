cef390cbc34cafe971d8fe0f0c0e1e631a645716
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-12-12 17:27:14 +0000

    #Classes and Objects Pt.1 Exercise #1~2

--- launch_school.rb
+++ launch_school.rb
@@ -13,3 +13,38 @@ class emotion
 end
 
 happy = emotion.new
+
+#Classes and Objects Pt.1 Exercise #1~2
+#Create MyCar class
+#Allow user to define year, color, and model of car
+#Create instance variable set to 0 during instantiation of object
+#to track current speed of car
+#Create instance methods that allow car to speed up, break, and shut off
+#Add accessor method to MyCar class to change and view color of car
+#Add accessor method to MyCar class to only view year of car
+class MyCar
+    attr_accessor :color
+    attr_reader :year
+
+    def initialize(year, color, model)
+        @year = year
+        @color = color
+        @model = model
+        @speed = 0
+    end
+
+    def speed_up(number)
+        @speed += number
+        puts "You are accelerating by #{number} mph"
+    end
+
+    def break(number)
+        @speed -= number
+        puts "You are de-celerating by #{number} mph"
+    end
+
+    def shutoff
+        @speed = 0
+        puts "Shutting down..."
+    end
+end
